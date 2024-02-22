88e8a207436ea3194be0a721a710de0d9948bf1c
Samit Mhatre <sm904139@dal.ca>
2024-02-21 22:42:05 -0400

    contact form

---
+++ frontend/falafellas/src/Components/Contact/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ContactForm from "./ContactSection";
+
+export default function Contact() {
+    return(
+            <div>
+                Contact Page
+                <section className="contact-section">
+                    <ContactForm/>
+                </section>
+            </div>
+
+    )
+}
