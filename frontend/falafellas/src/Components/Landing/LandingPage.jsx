f569b5d3fdbf52d13f5e94234003417fd25cf32b
Aakash Mukesh Nandwani <ak824757@dal.ca>
2024-02-27 20:43:55 -0400

    landing page changes

--- frontend/falafellas/src/Components/Landing/LandingPage.jsx
+++ frontend/falafellas/src/Components/Landing/LandingPage.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import './../../../src/common.css';
 import './LandingPage.css'
-import LandingPageImage from './LandingPageImage.png'
+import LandingPageImage from './LandingPageImage.jpg'
+import About from './About';
 
 function LandingPage() {
   return (
+    <div>
     <div className="landing-page">
       <div className="content-wrapper">
         <div className="text-container">
           <h2>Welcome to Falafellas Learning Hub</h2>
           <p>Empower your team with our comprehensive learning management system. Our platform allows you to efficiently manage, distribute, and monitor various training materials, fostering continuous growth and development among your workforce.</p>
-          <div className="cta-buttons">
-            <button className="get-started-button">Get Started</button>
+          <div className="about-buttons-container">
+            <button className="secondary-button">Get Started</button>
           </div>
         </div>
         <div className="image-container">
@@ -20,7 +22,18 @@ function LandingPage() {
         </div>
       </div>
     </div>
+
+    <About/>
+
+
+
+    </div>
+    
+
+
   );
+
+
 }
 
 export default LandingPage;
