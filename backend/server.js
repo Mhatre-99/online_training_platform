4697d57999c5d9e7f79ae3e3b17120042ac9957e
Samit Mhatre <sm904139@dal.ca>
2024-03-08 19:50:33 -0400

    adding backend boilerplate

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello, Express.js Server!</h1>');
+});
+
+const port = process.env.PORT || 3001; // You can use environment variables for port configuration
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
